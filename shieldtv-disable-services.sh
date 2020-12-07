9c9dec663640db3c239900d01e141cdf43f4db93
colino17 <51090962+colino17@users.noreply.github.com>
2020-12-07 15:30:22 -0400

    Update shieldtv-disable-services.sh

--- shieldtv-disable-services.sh
+++ shieldtv-disable-services.sh
@@ -31,9 +31,22 @@ adb shell "pm disable-user --user 0 com.android.printspooler"
 adb shell "pm disable-user --user 0 com.google.android.feedback"
 adb shell "pm disable-user --user 0 com.google.android.tv.bugreportsender"
 
-#adb shell "pm disable-user --user 0 
-#adb shell "pm disable-user --user 0 
-#adb shell "pm disable-user --user 0 
-#adb shell "pm disable-user --user 0 
-#adb shell "pm disable-user --user 0 
-#adb shell "pm disable-user --user 0 
+# Disable Game Streaming
+adb shell "pm disable-user --user 0 com.nvidia.geforcenow"
+adb shell "pm disable-user --user 0 com.nvidia.tegrazone3"
+
+# Disable Alexa Skill
+adb shell "pm disable-user --user 0 com.nvidia.shield.ask"
+
+# Disable Other Stuff
+adb shell "pm disable-user --user 0 android.autoinstalls.config.nvidia"
+adb shell "pm disable-user --user 0 com.android.htmlviewer"
+adb shell "pm disable-user --user 0 com.android.providers.calendar"
+adb shell "pm disable-user --user 0 com.android.providers.contacts"
+adb shell "pm disable-user --user 0 com.android.providers.userdictionary"
+adb shell "pm disable-user --user 0 com.google.android.marvin.talkback"
+adb shell "pm disable-user --user 0 com.google.android.partnersetup"
+adb shell "pm disable-user --user 0 com.google.android.speech.pumpkin"
+adb shell "pm disable-user --user 0 com.google.android.syncadapters.calendar"
+adb shell "pm disable-user --user 0 com.google.android.syncadapters.contacts"
+adb shell "pm disable-user --user 0 com.google.android.tvtutorials"
