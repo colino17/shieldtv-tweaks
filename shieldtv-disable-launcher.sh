df0daea06a170a28173e2780143df622ff87559d
colino17 <51090962+colino17@users.noreply.github.com>
2020-12-07 15:31:02 -0400

    Update shieldtv-disable-launcher.sh

--- shieldtv-disable-launcher.sh
+++ shieldtv-disable-launcher.sh
@@ -2,5 +2,7 @@
 
 # Disables the stock launcher
 # Make sure an alternate launcher is installed before running the script
+# adb shell "pm disable-user --user 0 com.google.android.leanbacklauncher"
 
 adb shell "pm disable-user --user 0 com.google.android.tvlauncher"
+
